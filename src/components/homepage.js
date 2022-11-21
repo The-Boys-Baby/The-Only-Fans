c3cb2a056f0b9547c7094aae815e5cf3c0cfacdc
ItsMashBro <M.Ashley365@Gmail.Com>
2022-11-21 15:45:33 -0600

    Router done

---
+++ src/components/homepage.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+
+
+
+const Homepage = () => {
+    return (
+        <div>
+            <div>
+                <h1>This is the damn homepage</h1>
+            </div>
+        </div>
+    )
+}
+
+
+export default Homepage;
---
+++ src/index.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import App from './app';
+import Homepage from './homepage'
+//other file imports go here
+
+
+const router = createBrowserRouter([
+    {
+        path: '/',
+        element: <App />,
+        errorElement: <Errorpage />,
+        children: [
+            {
+                index: true,
+                element: <Homepage />
+            },
+        ]
+    }
+])

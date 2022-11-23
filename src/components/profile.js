4e65d5f5a91f5c13b6f1bb55bc2635a66a0cf6dd
JeremyRogersFSA <jeremy.rogers@fullstackacademy.com>
2022-11-23 16:06:48 -0700

    code review

--- src/components/profile.js
+++ src/components/profile.js
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import { Link, useNavigate } from 'react-router-dom';
 
-
+//export default () => {}
 const ProfilePage = () => {
     const [personalProfile, setPersonalProfile] = useState([]);
     const [items, setItems] = useState([]);

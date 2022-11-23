96bb8dc47dac497de308d86adc6276925100c0bc
ItsMashBro <M.Ashley365@Gmail.Com>
2022-11-23 14:08:45 -0600

    Updated Router

--- src/index.js
+++ src/index.js
@@ -3,7 +3,10 @@ import ReactDOM from 'react-dom';
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import App from './app';
 import Homepage from './homepage'
-//other file imports go here
+import Errorpage from './components/errorpage';
+import Fans from './components/marketplace';
+import ProfilePage from './components/profile';
+
 
 
 const router = createBrowserRouter([
@@ -16,6 +19,18 @@ const router = createBrowserRouter([
                 index: true,
                 element: <Homepage />
             },
+            {
+                path: 'Errorpage',
+                element: <Errorpage />
+            },
+            {
+                path: 'Fans',
+                element: <Fans />
+            },
+            {
+                path: 'ProfilePage',
+                element: <ProfilePage />
+            }
         ]
     }
 ])

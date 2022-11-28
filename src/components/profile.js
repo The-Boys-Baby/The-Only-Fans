666561394e39fced5521c411401b8449bfe985f2
kgrif113 <91581102+kgrif113@users.noreply.github.com>
2022-11-28 17:11:16 -0500

    lots of stuff

--- src/components/profile.js
+++ src/components/profile.js
@@ -1,55 +1,51 @@
-import React, { useState, useEffect } from 'react';
-import { Link, useNavigate } from 'react-router-dom';
+import React, { useState, useEffect } from "react";
+import { Link, useNavigate } from "react-router-dom";
 
+const Profile = () => {
+  const [personalProfile, setPersonalProfile] = useState([]);
+  const [items, setItems] = useState([]);
+  const navigate = useNavigate();
 
-const ProfilePage = () => {
-    const [personalProfile, setPersonalProfile] = useState([]);
-    const [items, setItems] = useState([]);
-    const navigate = useNavigate();
+  function logOut(event) {
+    localStorage.removeItem("token");
+    navigate("/");
+  }
 
-
-
-
-    useEffect(() => {
-        if(localStorage.getItem("token")) {
-            async function fetchProfileData() {
-                try {
-                    if (!localStorage.getItem("token")) {
-                        navigate('/login')
-                    }
-                    const response = await fetch(
-                        {/* API Link Goes Here */}, {
-                            headers: {
-                                'Content-Type': 'application/json',
-                                'Authorization': `Bearer ${localStorage.getItem("token")}`
-                            }
-                        }
-                    );
-                    const data = await response.json();
-                    console.log("This is the profile data: ", data)
-                    setPersonalProfile(data)
-                } catch (error) {
-                    console.log(error)
-                    navigate('/profile')
-                }
-            }
-            fetchProfileData()
-
-            function logOut(event) {
-                localStorage.removeItem("token")
-                navigate("/")
-            }
+  useEffect(() => {
+    if (!localStorage.getItem("token")) {
+        navigate("/Login");
+      }
+    if (localStorage.getItem("token")) {
+      async function fetchProfileData() {
+        try {
+          const response = await fetch("https://localhost:1337/api/users/:id", {
+            headers: {
+              "Content-Type": "application/json",
+              Authorization: `Bearer ${localStorage.getItem("token")}`,
+            },
+          });
+          const data = await response.json();
+          console.log("This is the profile data: ", data);
+          setPersonalProfile(data);
+        } catch (error) {
+          console.log(error);
+          navigate("/profile");
         }
-    }, [])
+      }
+      fetchProfileData();
+    }
+  }, []);
 
-    return (
-        <div>
-            <div>
-                <h2>Your Profile</h2>
-                <button variant="contained" onClick={logOut}>Log Out</button>
-            </div>
-        </div>
-    )
-}
+  return (
+    <div>
+      <div>
+        <h2>Your Profile</h2>
+        <button variant="contained" onClick={logOut}>
+          Log Out
+        </button>
+      </div>
+    </div>
+  );
+};
 
-export default ProfilePage;
+export default Profile;

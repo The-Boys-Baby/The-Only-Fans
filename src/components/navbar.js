5dde6f15ef47633b585c3c9c0a4ee5aa9133cb36
ItsMashBro <M.Ashley365@Gmail.Com>
2022-11-30 16:52:29 -0600

    fans displayinggit add .

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -2,22 +2,14 @@ import React from "react";
 import { Link } from "react-router-dom";
 import OFlogo from "./OF.png";
 
-
 const Navbar = () => {
   return (
     <div>
       <div>
-        <h1>The Only Fans</h1>
-        <nav>
-          <Link to="/">   Home   </Link>
-          <Link to="Login">   Login   </Link>
-          <Link to="Profile">   Profile   </Link>
-          <Link to="Fans">   Marketplace   </Link>
-        </nav>
-        <div class="headerBox">
+        <div className="headerBox">
           <img src={OFlogo} />
         </div>
-        <div class="navBox">
+        <div className="navBox">
           <nav>
             <Link id="homenav" className="navItem" to="/">
               {"  "}
@@ -28,9 +20,9 @@ const Navbar = () => {
                 viewBox="0 0 24 24"
                 fill="none"
                 stroke="currentColor"
-                stroke-width="2"
-                stroke-linecap="round"
-                stroke-linejoin="round"
+                strokeWidth="2"
+                strokeLinecap="round"
+                strokeLinejoin="round"
               >
                 <path d="m3 9 9-7 9 7v11a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2z"></path>
                 <polyline points="9 22 9 12 15 12 15 22"></polyline>
@@ -46,9 +38,9 @@ const Navbar = () => {
                 viewBox="0 0 24 24"
                 fill="none"
                 stroke="currentColor"
-                stroke-width="2"
-                stroke-linecap="round"
-                stroke-linejoin="round"
+                strokeWidth="2"
+                strokeLinecap="round"
+                strokeLinejoin="round"
               >
                 <path d="M15 3h4a2 2 0 0 1 2 2v14a2 2 0 0 1-2 2h-4"></path>
                 <polyline points="10 17 15 12 10 7"></polyline>
@@ -64,9 +56,9 @@ const Navbar = () => {
                 viewBox="0 0 24 24"
                 fill="none"
                 stroke="currentColor"
-                stroke-width="2"
-                stroke-linecap="round"
-                stroke-linejoin="round"
+                strokeWidth="2"
+                strokeLinecap="round"
+                strokeLinejoin="round"
               >
                 <path d="M10.827 16.379a6.082 6.082 0 0 1-8.618-7.002l5.412 1.45a6.082 6.082 0 0 1 7.002-8.618l-1.45 5.412a6.082 6.082 0 0 1 8.618 7.002l-5.412-1.45a6.082 6.082 0 0 1-7.002 8.618l1.45-5.412Z"></path>
                 <path d="M12 12v.01"></path>
@@ -82,9 +74,9 @@ const Navbar = () => {
                 viewBox="0 0 24 24"
                 fill="none"
                 stroke="currentColor"
-                stroke-width="2"
-                stroke-linecap="round"
-                stroke-linejoin="round"
+                strokeWidth="2"
+                strokeLinecap="round"
+                strokeLinejoin="round"
               >
                 <path d="M6 2 3 6v14a2 2 0 0 0 2 2h14a2 2 0 0 0 2-2V6l-3-4z"></path>
                 <line x1="3" y1="6" x2="21" y2="6"></line>

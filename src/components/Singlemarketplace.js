eea90d680580963a0630104f2fc33a38490f9120
kgrif113 <91581102+kgrif113@users.noreply.github.com>
2022-12-07 22:55:52 -0500

    single product page

--- src/components/Singlemarketplace.js
+++ src/components/Singlemarketplace.js
@@ -1,92 +1,125 @@
-import React, { useState, useEffect } from 'react';
-import { Link, useParams } from 'react-router-dom';
-import routes from '../routes';
-
+import React, { useState, useEffect } from "react";
+import { Link, useParams, useOutletContext } from "react-router-dom";
+import routes from "../routes";
 
 const IndivFans = () => {
-    console.log("Fans should display")
-    const [fans, setFans] = useState([]);
-    const {devHost, marketplace} = routes
-    const { productId } = useParams()
-    const [quantity, setQuantity] = useState(1)
+  console.log("Fans should display");
+  const { devHost, marketplace } = routes;
+  const { productId } = useParams();
+  const [quantity, setQuantity] = useState(1);
+  const {
+    adminState: [isAdmin, setIsAdmin],
+    userState: [user, setUser],
+    fanState: [fans, setFans],
+  } = useOutletContext();
+  const [indivFan, setIndivFan] = useState(
+    fans.find((fan, index) => {
+      return productId == fan.id;
+    }) || {}
+  );
 
-    useEffect(() => {
-
-        async function postHandler() {
-        
-            try {
-                const response = await fetch(
-                    `${devHost}${marketplace}/${productId}`,
-                    {
-                        headers: {
-                            'Content-Type': 'application/json',
-                            'Authorization': `Bearer ${localStorage.getItem("token")}`
-                        }
-                    }
-                );
-                const data = await response.json();
-                console.log(data)
-                setFans(data);
-            } catch (error) {
-                console.log(error)
-            }
-        }
-        postHandler();
-        
-    }, [])
-    async function changeQuantity(event){
-        setQuantity(event.target.value)
-        console.log(quantity)
+  useEffect(() => {
+    async function postHandler() {
+      try {
+        const response = await fetch(`${devHost}${marketplace}/${productId}`, {
+          headers: {
+            "Content-Type": "application/json",
+            Authorization: `Bearer ${localStorage.getItem("token")}`,
+          },
+        });
+        const data = await response.json();
+        setIndivFan(data);
+      } catch (error) {
+        console.log(error);
+      }
     }
-    async function submitButton(){
-        try {
-            console.log(`${devHost}${marketplace}/${productId}`)
-        
-            const response = await fetch(
-                `${devHost}${marketplace}/${productId}`,
-                {
-                    method: "POST",
-                    headers: {
-                        'Content-Type': 'application/json',
-                        'Authorization': `Bearer ${localStorage.getItem("token")}`
-                    }, body: JSON.stringify({
-                        quantity: quantity
-                    })
-                }
+    postHandler();
+  }, []);
+  async function changeQuantity(event) {
+    setQuantity(event.target.value);
+    console.log(quantity);
+  }
+  async function submitButton() {
+    try {
+      console.log(`${devHost}${marketplace}/${productId}`);
+
+      const response = await fetch(`${devHost}${marketplace}/${productId}`, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: `Bearer ${localStorage.getItem("token")}`,
+        },
+        body: JSON.stringify({
+          quantity: quantity,
+        }),
+      });
+      const data = await response.json();
+      console.log(data);
+    } catch (error) {
+      console.log(error);
+    }
+  }
+  return (
+    <div className="singleProdApp">
+      <div className="singleProdBox">
+        <div className="singleProd">
+          {indivFan?.id ? (
+            <div>
+              <div key={indivFan.id}>
+                <h2>{indivFan.name}</h2>
+                <p>${indivFan.price}</p>
+                <p>{indivFan.description}</p>
+                <div>
+                  <img
+                    className="singleProdPhoto"
+                    src={indivFan.pictures}
+                    alt={indivFan.name}
+                  />
+                </div>
+                <button onClick={submitButton}>Add To Cart</button>
+                <select
+                  name="fan"
+                  id="quantity"
+                  value={quantity}
+                  onChange={changeQuantity}
+                >
+                  <option value={1}>1</option>
+                  <option value={2}>2</option>
+                  <option value={3}>3</option>
+                  <option value={4}>4</option>
+                  <option value={5}>5</option>
+                  <option value={10}>10</option>
+                  <option value={15}>15</option>
+                  <option value={20}>20</option>
+                  <option value={25}>25</option>
+                  <option value={100}>100</option>
+                </select>
+              </div>
+            </div>
+          ) : (
+            "Aint Nothin"
+          )}
+          {console.log(fans)}
+        </div>
+      </div>
+      {fans &&
+        fans.map((notFan, idx) => {
+          if (notFan.id != indivFan.id) {
+            return (
+              <div key={idx} className="prodBar">
+                  <img
+                    className="fanPics"
+                    src={notFan.pictures}
+                    alt={notFan.name}
+                  />
+                  <p>{notFan.name} </p>
+                  <p>{notFan.price} </p>
+              </div>
             );
-            const data = await response.json();
-            console.log(data)
-        } catch (error) {
-            console.log(error)
-        }
-    }
-    return (
-         <div>
-                {fans ? <div>
-                    <div key={fans.id}>
-                        <h2>{fans.name}</h2>
-                        <p>{fans.price}</p>
-                        <p>{fans.description}</p>
-                        <div><img src={fans.pictures} alt={fans.name}/></div>
-                        <button onClick={submitButton}>Add To Cart</button>
-                        <select name = "fan" id = "quantity" value = {quantity} onChange={changeQuantity}>
-                            <option value= {1}>1</option>
-                            <option value= {2}>2</option>
-                            <option value= {3}>3</option>
-                            <option value= {4}>4</option>
-                            <option value= {5}>5</option>
-                            <option value= {10}>10</option>
-                            <option value= {15}>15</option>
-                            <option value= {20}>20</option>
-                            <option value= {25}>25</option>
-                            <option value= {100}>100</option>
-                        </select>
-                    </div> 
-            </div> : "Aint Nothin" }
-        </div> 
-    )
-}
-
-
+          }
+        })}
+    </div>
+  );
+};
 
 export default IndivFans;
